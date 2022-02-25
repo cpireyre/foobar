acffef845806029afab06636bea95e90afd6b0fe
Colin <cpireyre@student.42.fr>
2022-02-25 01:53:38 +0100

    better

--- find-the-access-codes.py
+++ find-the-access-codes.py
@@ -1,37 +1,43 @@
 from pprint import pprint
 def insert(trie, n):
-    divisors = [m for m in trie.viewkeys() - {"end"} if n % m == 0]
-    curr = trie.get("end", 0)
-    pprint("n = %d, curr = %d" % (n, curr))
+    divisors = [d for d in trie.viewkeys() - {"end"} if n % d == 0]
     if not divisors:
         trie[n] = {"end": 1}
-        return curr
-    elif n in divisors:
-        trie[n]["end"] += 1
-        return curr
     else:
-        return curr + sum(insert(trie[d], n) for d in divisors)
+        for d in divisors:
+            insert(trie[d], n)
 
 from math import factorial
-def kchoose2(k):
-    return 0 if k < 2 else factorial(k) / 2 * factorial(k - 2)
+def nchoosek(n, k):
+    if n < k:
+        return 0
+    elif n == k:
+        return 1
+    else:
+        return (factorial(n)) / (factorial(k) * (factorial(n - k)))
+
+def search(trie, n, depth=0):
+    divisors = [d for d in trie.viewkeys() - {"end"} if n % d == 0]
+    if not divisors:
+        return nchoosek(depth, 2)
+    else:
+        return sum(search(trie[d], n, depth + 1) for d in divisors)
 
 def solution(ns):
     trie = dict()
     acc = 0
     for n in ns:
-        tmp = insert(trie, n)
-        print("n = %d, insert = %d" % (n, tmp))
-        acc += kchoose2(tmp)
-    pprint(trie)
+        s = search(trie, n)
+        acc += s
+        insert(trie, n)
     return acc
 
 # ns = [1,2,4] # 1
 # print(solution(ns))
 # ns = [1,1,2] # 1
 # print(solution(ns))
-ns = [1,2,2] # 1
-print(solution(ns))
+# ns = [9,3,9,9] # 2
+# print(solution(ns))
 # ns = [1,1,1] # 1
 # print(solution(ns))
 # ns = [1,1] # 0
