e953c3510d6d10c5c8bca06c63b2803639118453
Colin <cpireyre@student.42.fr>
2022-03-13 13:46:09 +0100

    should be good but isn't and idk why

--- bringing-a-gun-to-a-trainer-fight.py
+++ bringing-a-gun-to-a-trainer-fight.py
@@ -1,170 +1,90 @@
-from itertools import product
+# all the coprime translations of alpha by (W * x, H * y), x, y in Z
+# with norm less than D
+# minus the ones with smaller period than D?
+# just gen everyone and compute with gcd
+# unless you were multiples with (W, 0), (0, H) or (W, H)
+# i.e. everyone where W | x or H | y
+# and only need to test half of them because???
 from pprint import pprint
+
+# |T| = sqrt(ax**2 + W**2 x**2 + ay**2 +  H**2 y**2)
+# ax^2 + W^2 x^2 + ay^2 + H^2 y^2 < D^2
+# W^2 x^2 + H^2 y^2 < D^2 - ax^2 - ay^2
+from itertools import product
+
+
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
+
+
 def sign(x):
     return 1 if x > 0 else -1
+
+
+from math import atan2, hypot
 def solution(dimensions, shooter, target, distance):
-    width, height = dimensions[0], dimensions[1]
-    boundx, boundy = 1 + distance // width, 1 + distance // height
-    rangex, rangey = range(-boundx, boundx + 1), range(-boundy, boundy + 1)
-    (shooterx, shootery), (targetx, targety) = shooter, target
-    bx, by = width - targetx, height - targety
-    fx, fy = width - shooterx, height - shootery
-    alphax, alphay = targetx - shooterx, targety - shootery
-    Bx = [x for k in rangex for x in (2*k*width + alphax, 2*k*width + alphax + 2 * bx)]
-    By = [y for k in rangex for y in (2*k*height+ alphay, 2*k*height + alphay + 2 * by)]
-    Fx = [x for k in rangex for x in (2*k*width, 2*k*width + 2 * fx)]
-    Fy = [y for k in rangex for y in (2*k*height, 2*k*height + 2 * fy)]
+    W, H = dimensions
+    shooterx, shootery = shooter
+    boundx, boundy = 50, 50
 
     def M(x, y):
-        return x**2 + y**2
+        # return hypot(x - shooterx, y - shootery)
+        return (x - shooterx) ** 2 + (y - shootery) ** 2
+    distance *= distance
 
     def norm(x, y):
-        if not x and not y:
-            return (0, 0)
-        xSign, ySign = sign(x), sign(y)
-        x, y = abs(x), abs(y)
-        divisor = gcd(x, y)
-        return (xSign * x // divisor, ySign * y // divisor)
-
-    A = dict()
-    distance *= distance
-    for f in product(Fx, Fy):
-        fnorm, fmetric = norm(*f), M(*f)
-        if fmetric <= distance and (fnorm not in A or A[fnorm][0] > fmetric):
-            A[fnorm] = (fmetric, False)
-    for b in product(Bx, By):
-        bnorm, bmetric = norm(*b), M(*b)
-        if bmetric <= distance and (bnorm not in A or A[bnorm][0] > bmetric):
-            A[bnorm] = (bmetric, True)
-
-    return set(k for k, (m, v) in A.items() if v)
-    # return len(list(k for k, (m, v) in A.items() if v))
-
-N, S, E, W = (0, -1), (0, 1), (1, 0), (-1, 0)
-NE, NW, SE, SW = (1, -1), (-1, -1), (1, 1), (-1, 1)
-reflections = {(0, 0): (N, S, E, W, NE, NW, SE, SW),
-        N:(N, S, E, W, NE, NW, SE, SW),
-        S:(N, S, E, W, NE, NW, SE, SW),
-        E:(N, S, E, W, NE, NW, SE, SW),
-        W:(N, S, E, W, NE, NW, SE, SW),
-        NE:(N, S, E, W, NE, NW, SE, SW),
-        NW:(N, S, E, W, NE, NW, SE, SW),
-        SE:(N, S, E, W, NE, NW, SE, SW),
-        SW:(N, S, E, W, NE, NW, SE, SW),
-        }
-
-from collections import namedtuple
-from heapq import heapify, heappop, heappush
-from pprint import pprint
-
-
-def solution2(dimensions, shooter, target, distance):
-    width, height = dimensions
-
-    distance *= distance
-    shooterx, shootery = shooter[0], shooter[1]
-    def M(x, y):
+        # return atan2(x - shooterx, y - shootery)
         x, y = x - shooterx, y - shootery
-        return x**2 + y**2
-
-    def norm(x, y):
-        x, y = x - shooterx, y - shootery
-        if not x and not y:
+        if (x, y) == (0, 0):
             return (0, 0)
-        xSign, ySign = sign(x), sign(y)
+        signx, signy = sign(x), sign(y)
         x, y = abs(x), abs(y)
-        divisor = gcd(x, y)
-        return (xSign * x // divisor, ySign * y // divisor)
+        g = gcd(x, y)
+        return (signx * (x // g), signy * (y // g))
 
-
-    Bogey = namedtuple("Bogey", "metric isHostile direction angle x y xOffset yOffset")
-
-    def bogey(x, y, isHostile, direction=(0, 0)):
-        return Bogey(
-            M(x, y), isHostile, direction, norm(x, y), x, y, (x, width - x), (y, height - y) 
+    def S(x, y):
+        xoffset, yoffset = W - x, H - y
+        return (
+            (x, y),
+            (x + 2 * xoffset, y),
+            (x, y + 2 * yoffset),
+            (x + 2 * xoffset, y + 2 * yoffset),
         )
 
-    shooter = bogey(*shooter, isHostile=False)
-    target = bogey(*target, isHostile=True)
-    if target.metric > distance:
-        return 0
+    friendlies = S(*shooter)
+    bogeys = S(*target)
 
-    def reflect(b, horizontal=1, vertical=1):
-        nX = b.x + horizontal * 2 * b.xOffset[max(0, horizontal)]
-        nY = b.y + vertical * 2 * b.yOffset[max(0, vertical)]
-        nXOffset = (b.xOffset[1], b.xOffset[0]) if horizontal else b.xOffset
-        nYOffset = (b.yOffset[1], b.yOffset[0]) if vertical else b.yOffset
-        return Bogey(
-            M(nX, nY), b.isHostile, (horizontal, vertical), norm(nX, nY), nX, nY, nXOffset, nYOffset 
-            )
+    A = dict()
+    def translate((Tx, Ty), (x, y)):
+        return (2 * W * Tx + x, 2 * Ty * H + y)
+    def shoot(T, bogeys, isHostile):
+        for b in (translate(T, b) for b in bogeys):
+            bnorm, bmetric = norm(*b), M(*b)
+            if bmetric <= distance and (bnorm not in A or A[bnorm][0] > bmetric):
+                A[bnorm] = (bmetric, isHostile)
 
-    def neighbors(bogey):
-        return (reflect(bogey, *r) for r in reflections[bogey.direction])
-
-    Q = [target, shooter]
-    heapify(Q)
-    seenHostile = {target.angle}
-    seenFriendly = {shooter.angle}
-    # pprint(target)
-    while Q:
-        curr = heappop(Q)
-        # print("Reflections of %s, metric %d" % ((curr.x, curr.y),curr.metric))
-        for img in neighbors(curr):
-            if img.metric <= distance and img.angle not in seenHostile and img.angle not in seenFriendly:
-                # pprint(img)
-                heappush(Q, img)
-                if img.angle in ((65,76),(77,64)):
-                    print(img)
-                (seenHostile if img.isHostile else seenFriendly).add(img.angle)
-    return seenHostile
+    for T in product(range(-boundx, boundx + 1), range(-boundy, boundy + 1)):
+        shoot(T, friendlies, False)
+        shoot(T, bogeys, True)
+    pprint(A)
+    return [k for k, v in A.items() if v[1]]
 
 
-# S = solution((3,2), (1,1), (2,1), 4) # 7
-# pprint(S)
-
-# S = solution((3,2), (1,1), (2,1), 500) 
-# Sprime = solution2((3,2), (1,1), (2,1), 500) 
-# pprint(Sprime - S)
-# pprint(S - Sprime)
-# print(len(Sprime))
-# print(len(S))
+# So = solution((3,2), (1,1), (2,1), 4) # 7
+# So = solution((3,2), (2,1), (1,1), 100) # 3995
+# So = solution((3,2), (2,1), (1,1), 500) # 99465
+So = solution((300, 275), (150, 150), (185, 150), 500) # 9
+# pprint(So)
+print(len(So))
 # import matplotlib.pylab as plt
-# matrix = [[0 for _ in range(1000)] for _ in range(1000)]
-# offset = 500
+
+# matrix = [[0 for _ in range(300)] for _ in range(300)]
+# offset = 150
 # matrix[offset][offset] = 2
 # for x, y in S:
 #     matrix[offset + y][offset + x] = 1
+# print(len(S))
 # plt.spy(matrix, marker=".", markersize=2)
 # plt.show()
-
-# dimensions = (3, 2)
-# me = (1, 1)
-# trainer = (2, 1)
-# distance = 100
-# S = solution(dimensions, me, trainer, distance) # 3995, 0.17s
-# print(len(S))
-
-# dimensions = (3, 2)
-# me = (1, 1)
-# trainer = (2, 1)
-# distance = 220
-# S = solution(dimensions, me, trainer, distance) # 19265, 0.61s
-# print(len(S))
-# pprint(S)
-
-# S = solution((3,2), (1,1), (2,1), 500) # 99465, 3.12s
-# # assert(S) == 99465
-# pprint(S)
-
-# dimensions = (300, 275)
-# me = (150, 150)
-# trainer = (185, 100)
-# distance = 500
-# S = solution(dimensions, me, trainer, distance) # 9
-# Sprime = solution2(dimensions, me, trainer, distance) # 9
-# pprint(S)
-# pprint(Sprime)
