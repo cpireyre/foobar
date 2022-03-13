3972ebc4e47816aae05097005a23eec0882c545d
Colin <cpireyre@student.42.fr>
2022-03-13 22:43:25 +0100

    more cleaning up and refactoring

--- bringing-a-gun-to-a-trainer-fight.py
+++ bringing-a-gun-to-a-trainer-fight.py
@@ -1,75 +1,68 @@
+# The basic idea is we unfold the billiard into a torus then tile
+# the plane (up to a somewhat arbitrary boundary) with that torus
+# and compute all the bogeys, then filter with respect to distance
+# from origin and unicity of angle (which we check with the gcd
+# of the x and y components).
+
+# No floats, no sqrt, no atan2, all integers.
 from itertools import product
-
-
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-
-def sign(x):
-    return 1 if x >= 0 else -1
-
-
 def solution(dimensions, shooter, target, distance):
     W, H = dimensions
+    boundx, boundy = 2 + distance // (2 * W), 2 + distance // (2 * H)
     shooterx, shootery = shooter
-    boundx, boundy = 2 + distance // W, 2 + distance // H
 
-    distance *= distance
     def M(x, y):
+        """Returns a measure of the distance between (x, y) and the shooter."""
         return (x - shooterx) ** 2 + (y - shootery) ** 2
+    distance *= distance # Only using squared distances from now on
 
+    def gcd(a, b): # Not in the math module in Python 2.7 for some reason
+        while b:
+            a, b = b, a % b
+        return a
+
+    sign = lambda x: 1 if x >= 0 else -1
     def norm(x, y):
-        x, y = x - shooterx, y - shootery
-        if (x, y) == (0, 0):
+        """Normalizes a vector to coprime components, with respect
+        to the shooter, preserves orientation."""
+        if (x, y) == (shooterx, shootery):
             return (0, 0)
+        x, y = x - shooterx, y - shootery
         signx, signy = sign(x), sign(y)
         x, y = abs(x), abs(y)
         g = gcd(x, y)
         return (signx * (x // g), signy * (y // g))
 
-    def S(x, y):
+    def images(x, y):
+        """Returns the 4 reflections of (x, y) in the unfolded torus
+        corresponding to the rational billiard on the rectangle (W, H)."""
         xoffset, yoffset = W - x, H - y
-        return (
-            (x, y),
-            (x + 2 * xoffset, y),
-            (x, y + 2 * yoffset),
-            (x + 2 * xoffset, y + 2 * yoffset),
-        )
+        return ((x, y), (x + 2 * xoffset, y), (x, y + 2 * yoffset),
+            (x + 2 * xoffset, y + 2 * yoffset))
 
-    friendlies = S(*shooter)
-    bogeys = S(*target)
+    A, friendlies, bogeys = dict(), images(*shooter), images(*target)
 
-    A = dict()
+    # I don't totally like how this function mutates in place an out-of-scope object
     def shoot(T, bogeys, isHostile):
-        Tx, Ty = T
-        xoffset, yoffset = 2 * W * Tx, 2 * H * Ty
+        """Adds the new angles to the dict A assuming they are smaller than any
+        previously seen for a given trajectory."""
+        xoffset, yoffset = 2 * W * T[0], 2 * H * T[1]
         for b in ((x + xoffset, y + yoffset) for x, y in bogeys):
-            bnorm, bmetric = norm(*b), M(*b)
-            if bmetric <= distance and (bnorm not in A or A[bnorm][0] > bmetric):
-                A[bnorm] = (bmetric, isHostile)
+            angle, bmetric = norm(*b), M(*b)
+            if bmetric <= distance and (angle not in A or A[angle][0] > bmetric):
+                A[angle] = (bmetric, isHostile)
 
-    for T in product(range(-boundx, boundx + 1), range(-boundy, boundy + 1)):
+    for T in product(range(-boundx, boundx), range(-boundy, boundy)):
         shoot(T, friendlies, False)
         shoot(T, bogeys, True)
+
     return len([k for k, v in A.items() if v[1]])
 
 
-# So = solution((3,2), (1,1), (2,1), 4) # 7
-So = solution((3,2), (2,1), (1,1), 100) # 3995
-# So = solution((3,2), (2,1), (1,1), 500) # 99465
-# So = solution((300, 275), (150, 150), (180, 100), 500) # 9
-# So = solution((300, 275), (150, 150), (180, 100), 0) # 0
-# So = solution((1250, 1250), (1000, 1000), (500, 400), 10000) # 196
-print(So)
-# import matplotlib.pylab as plt
-
-# matrix = [[0 for _ in range(300)] for _ in range(300)]
-# offset = 150
-# matrix[offset][offset] = 2
-# for x, y in S:
-#     matrix[offset + y][offset + x] = 1
-# print(len(S))
-# plt.spy(matrix, marker=".", markersize=2)
-# plt.show()
+# So = solution((3,2), (1,1), (2,1), 4) # 7, 0.06s
+# So = solution((3,2), (2,1), (1,1), 100) # 3995, 0.08s
+# So = solution((3,2), (2,1), (1,1), 500) # 99463, 0.67s
+# So = solution((300, 275), (150, 150), (180, 100), 500) # 9, 0.06s
+# So = solution((300, 275), (150, 150), (180, 100), 0) # 0, 0.06s
+# So = solution((1250, 1250), (1000, 1000), (500, 400), 10000) # 196, 0.06s
+# print(So)
