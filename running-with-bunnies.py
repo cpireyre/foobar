69190d27e93d34974a7e08673f5908048b9a4617
Colin <cpireyre@student.42.fr>
2022-03-18 18:25:56 +0100

    clearer comments

--- running-with-bunnies.py
+++ running-with-bunnies.py
@@ -1,15 +1,3 @@
-# think i got it... it's not hard. use johnson's algorithm (modified bellman-ford)
-# (or floyd-warshall)
-# to compute min cost for all pairs of vertices. if there is a negative cycle, save
-# everyone and exit right away.
-# if there are no negative cycles, number your bunnies 1-5, say source = 0 and sink = 6, then:
-# a rescue path is a sequence like 0 3 2 4 6 (always 0 then some number of bunnies then 6)
-# which means if you gen the set of partial permutations of {1 2 3 4 5} you can list all the possible
-# rescue paths. then you pairwise, map cost, reduce +, filter out everyone with cost > time_limit,
-# group by len, max, sort, output.
-# it's OK to compute all this b/c worst case it's like 100 bellman-fords + about 1000 possible paths
-# (actually less but approx. this order of magnitude on both counts)
-
 from itertools import product, permutations, chain, tee
 
 
@@ -25,40 +13,21 @@ def partialPermutations(xs):
 
 def solution(G, limit):
     source, sink = 0, len(G) - 1
-    V, bunnies = xrange(len(G)), xrange(1, sink)
+    V, bunnies = range(sink + 1), range(1, sink)
+
+    # Computing the min cost transitive closure of G with Floyd-Warshall:
     for k, i, j in product(V, V, V):
         G[i][j] = min(G[i][j], G[i][k] + G[k][j])
-        if G[i][i] < 0:
+        if G[i][i] < 0: # if G contains a negative cycle, we can save everyone
             return [bunny - 1 for bunny in bunnies]
 
     def cost(path):
         return sum(G[u][v] for u, v in pairwise((source,) + path + (sink,)))
 
+    # Checking all paths. Note that permutations() emits in lexicographic order.
     best = ()
     for path in partialPermutations(bunnies):
-        if cost(path) <= limit:
-            best = path if len(path) > len(best) else min(best, path)
+        if cost(path) <= limit and len(path) > len(best):
+            best = path
+
     return [bunny - 1 for bunny in sorted(best)]
-
-
-# times, time = [
-#     [0, 2, 2, 2, -1],
-#     [9, 0, 2, 2, -1],
-#     [9, 3, 0, 2, -1],
-#     [9, 3, 2, 0, -1],
-#     [9, 3, 2, 2, 0],
-# ], 1  # [1, 2]
-# times, time = [[-20, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], -1000 # [0, 1, 2]
-times, time = [
-    [0, 1, 1, 1, 1],
-    [1, 0, 1, 1, 1],
-    [1, 1, 0, 1, 1],
-    [1, 1, 1, 0, 1],
-    [1, 1, 1, 1, 0],
-], 3  # [0, 1]
-# times, time = [[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 100 # [0, 1, 2], 0.05s
-# times, time = [[0, 1], [1, 0]], 10 # []
-# times, time = [[-10, 1], [1, 0]], 10 # []
-# times, time = [[2, 1], [1, 0]], -30 # []
-S = solution(times, time)
-print(S)
