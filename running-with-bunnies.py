2269469c13be56636012f86ecb7dec0f9fac7f16
Colin <cpireyre@student.42.fr>
2022-03-18 17:38:24 +0100

    refactor the best path computation

--- running-with-bunnies.py
+++ running-with-bunnies.py
@@ -14,21 +14,16 @@ from itertools import product, permutations, chain, tee
 
 
 def pairwise(xs):  # itertools.pairwise() was introduced in Python 3.10
-    """pairwise('ABCDEFG') --> AB BC CD DE EF FG"""
     a, b = tee(xs)
     next(b, None)
     return zip(a, b)
 
 
 def partialPermutations(xs):
-    s = list(xs)
-    return chain.from_iterable(permutations(s, r) for r in range(len(s) + 1))
+    return chain.from_iterable(permutations(xs, r) for r in range(len(xs) + 1))
 
 
-from pprint import pprint
-
-
-def solution(G, times_limit):
+def solution(G, limit):
     source, sink = 0, len(G) - 1
     V, bunnies = xrange(len(G)), xrange(1, sink)
     for k, i, j in product(V, V, V):
@@ -36,36 +31,34 @@ def solution(G, times_limit):
         if G[i][i] < 0:
             return [bunny - 1 for bunny in bunnies]
 
-    def pathCost(path):
+    def cost(path):
         return sum(G[u][v] for u, v in pairwise((source,) + path + (sink,)))
 
-    paths = {
-        p: pathCost(p)
-        for p in partialPermutations(bunnies)
-        if pathCost(p) <= times_limit
-    }
-    if not paths:
-        return []
-    best = max(paths.keys(), key=len)
-    candidates = [path for path in paths if len(path) == len(best)]
-    pprint(candidates)
-    pprint(paths)
-    res = sorted(candidates)[0]
-    return sorted([bunny - 1 for bunny in res])
+    best = ()
+    for path in partialPermutations(bunnies):
+        if cost(path) <= limit:
+            best = path if len(path) > len(best) else min(best, path)
+    return [bunny - 1 for bunny in sorted(best)]
 
 
-times, time = [
-    [0, 2, 2, 2, -1],
-    [9, 0, 2, 2, -1],
-    [9, 3, 0, 2, -1],
-    [9, 3, 2, 0, -1],
-    [9, 3, 2, 2, 0],
-], 1  # [1, 2]
+# times, time = [
+#     [0, 2, 2, 2, -1],
+#     [9, 0, 2, 2, -1],
+#     [9, 3, 0, 2, -1],
+#     [9, 3, 2, 0, -1],
+#     [9, 3, 2, 2, 0],
+# ], 1  # [1, 2]
 # times, time = [[-20, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], -1000 # [0, 1, 2]
-# times, time = [[0, 1, 1, 1, 1], [1, 0, 1, 1, 1], [1, 1, 0, 1, 1], [1, 1, 1, 0, 1], [1, 1, 1, 1, 0]], 3 # [0, 1]
+times, time = [
+    [0, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1],
+    [1, 1, 0, 1, 1],
+    [1, 1, 1, 0, 1],
+    [1, 1, 1, 1, 0],
+], 3  # [0, 1]
 # times, time = [[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 100 # [0, 1, 2], 0.05s
 # times, time = [[0, 1], [1, 0]], 10 # []
 # times, time = [[-10, 1], [1, 0]], 10 # []
 # times, time = [[2, 1], [1, 0]], -30 # []
 S = solution(times, time)
-pprint(S)
+print(S)
